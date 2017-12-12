82bc9e7d9eca3d9f4d6b2330f45e0703aea4feaf
anaseinea <u14111377@sharjah.ac.ae>
2017-12-12 23:07:13 +0400

    adding remove button and tags

--- addToKart.php
+++ addToKart.php
@@ -35,7 +35,7 @@
         
         <nav class="red">
             <div class="nav-wrapper container">
-                <a href="index.html" class="brand-logo center"><img src="/Dev_of_Web/Pics/Logo.png" width="166" height="62"></a>
+                <a href="index.php" class="brand-logo center"><img src="/Dev_of_Web/Pics/Logo.png" width="166" height="62"></a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="left hide-on-med-and-down">
                     <li><a href="About.html">About</a></li>
@@ -61,33 +61,58 @@
         <div class="row container">
 <?php 
             DEFINE ('DB_USER', 'root');
-        DEFINE ('DB_PASSWORD', 'mypassword');
-        DEFINE ('DB_HOST', 'localhost');
-        DEFINE ('DB_NAME', 'products');
-$connection = mysqli_connect (DB_HOST, DB_USER, "", DB_NAME);
-        $prods = $connection->query("SELECT * FROM products");
-session_start();
+            DEFINE ('DB_PASSWORD', 'mypassword');
+            DEFINE ('DB_HOST', 'localhost');
+            DEFINE ('DB_NAME', 'products');
+            $connection = mysqli_connect (DB_HOST, DB_USER, "", DB_NAME);
+            $prods = $connection->query("SELECT * FROM products");
+            session_start();
             $total = 0;
-
-        for ($i =0;$i<mysqli_num_rows($prods) ; $i++){
-        $prods2 = mysqli_fetch_array($prods);
-            if(isset($_POST["check".$i])){
-                $total += $prods2["price"];
-                if(!isset($_SESSION['kart']))
-                    $_SESSION['kart']="";
-                $_SESSION['kart'] = $_SESSION['kart'].",".$i;
-       echo "<div class='col s12'>" . "<div class='card horizontal'>" . "<div class='card-image'>"
-            . "<img style='height:130px;' src='/Dev_of_Web/Pics/".$prods2["image"]."'></div>"
-                . "<div class='card-stacked'><div class='card-content'><p>"
-                .  $prods2["descriptionS"]
-                . "</p><p class='right-align'>".$prods2["price"]." AED</p></div>"
-                ."<div class='card-action'>"
-                . "<a onclick='removeFromKart(".$i.")' href='#' class='red-text center-align'>Remove item</a>"
-                . "</div></div></div></div>";
+            
+            if(isset($_GET['remove'])){
+                $_SESSION['kart'] = str_replace($_GET['remove'],"",$_SESSION['kart']);
             }
+            
+            if(!isset($_SESSION['kart']))
+                $_SESSION['kart']="";
+            //print_r($_SESSION);
+            
+            for ($i =0;$i<mysqli_num_rows($prods) ; $i++){
+                
+                if(isset($_POST["check".$i])){
+                    
+                    $_SESSION['kart'] = $_SESSION['kart'].",".$i;
+                    
+            }
+            
+            }//$_SESSION['kart']=""; 
+            
+                $p = explode(",",$_SESSION['kart']);
+                
+                $prods2 = mysqli_fetch_all($prods,MYSQLI_ASSOC);
+            //print_r($p);
+                for($i=1; $i<count($p) ;$i++){
+                    if($p[$i] == "")
+                        continue;
+                    //echo "index ".$p[$i];
+                    $total += $prods2[$p[$i]]["price"];
+                    echo "<div class='col s12'>" . "<div class='card horizontal'>" . "<div class='card-image'>"
+                        . "<img style='height:130px;' src='/Dev_of_Web/Pics/".$prods2[$p[$i]]["image"]."'></div>"
+                        . "<div class='card-stacked'><div class='card-content'><p>"
+                        .  $prods2[$p[$i]]["descriptionS"]
+                        . "</p><p class='right-align'>".$prods2[$p[$i]]["price"]." AED</p></div>"
+                        ."<div class='card-action'>"
+                        . "<a  href='addToKart.php?remove=".$p[$i]."' class='red-text center-align'>Remove item</a>"
+                        . "</div></div></div></div>";
+                }
+                
+                    
+            
+
+            
            
-        }
-            echo $total;
+        
+            echo "<p class='center-align'>Total amount: ".$total."<br/>Your balance: "./*snap.val().*/"</p>";
 
 
 
